4634dcb626e66a4daedbf83eac9174ac42fb14fe
Alexandra <manik167@gmail.com>
2025-11-09 11:59:24 +0100

    android emulator fixed

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -23,6 +23,6 @@ tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
 
-dependencies {
-    classpath(com.google.gms:google-services:4.3.15)
+plugins  {
+    id("com.google.gms.google-services") version "4.3.15" apply false
 }
